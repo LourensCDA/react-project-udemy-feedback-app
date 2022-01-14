520f8d4f9ca66c8b3e184fc782dc2dad7655837b
Lourens Botha <56141562+LourensCDA@users.noreply.github.com>
2022-01-14 04:23:04 +0200

    section 12

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import reactDom from 'react-dom';
 import './index.css';
 import App from './App';
+import { StrictMode } from 'react/cjs/react.production.min';
 
 reactDom.render(
   <React.StrictMode>
