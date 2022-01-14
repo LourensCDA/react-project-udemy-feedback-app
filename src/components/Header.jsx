65956d701025f4fb1c850187143c70b5294c9407
Lourens Botha <56141562+LourensCDA@users.noreply.github.com>
2022-01-14 04:27:36 +0200

    section 13 styles declared in component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
 import PropTypes from 'prop-types';
 
 function Header({ text }) {
+  const headerStyles = {
+    backgroundColor: 'blue', 
+    color: 'red'
+  }
+
   return (
-    <header>
+    <header style={headerStyles}>
       <div className='container'>
         <h2>{text}</h2>
       </div>
