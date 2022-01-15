2d8212e61e123993781e16463bf5b5fc0fc72361
Lourens Botha <56141562+LourensCDA@users.noreply.github.com>
2022-01-15 06:28:03 +0200

    Section 15

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,11 +1,13 @@
+import FeedbackItem from './FeedbackItem'
+
 function FeedbackList({ feedback }) {
   if (!feedback || feedback.length === 0) {
-    return 'No feedback yet'
+    return <p>No feedback yet</p>
   }
   return (
     <div className='feedback-list'>
       {feedback.map((item) => (
-        <div>{item.rating}</div>
+        <FeedbackItem key={item.id} item={item} />
       ))}
     </div>
   )
