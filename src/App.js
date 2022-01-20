b5b23a0d85adb7467cfa9e5c8bd1b003f58e0858
Lourens Botha <56141562+LourensCDA@users.noreply.github.com>
2022-01-20 04:50:49 +0200

    section 28

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import { v4 as uuidv4 } from 'uuid'
 import { useState } from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  NavLink,
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Card from './components/shared/Card'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
@@ -52,14 +47,6 @@ function App() {
 
           <Route path='/about' element={<AboutPage />} />
         </Routes>
-        <Card>
-          <NavLink to='/' activeClassName='active'>
-            Home
-          </NavLink>
-          <NavLink to='/about' activeClassName='active'>
-            About
-          </NavLink>
-        </Card>
 
         <AboutIconLink />
       </div>
